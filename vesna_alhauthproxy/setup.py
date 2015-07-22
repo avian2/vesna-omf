9fb4f173d1312d3898047a759310d5e9a2f9eb67
Tomaz Solc <tomaz.solc@tablix.org>
2015-07-22 12:09:34 +0200

    Renamed init script

--- vesna_alhauthproxy/setup.py
+++ vesna_alhauthproxy/setup.py
@@ -7,7 +7,7 @@ def get_long_description():
 	return open(os.path.join(os.path.dirname(__file__), "README.rst")).read()
 
 setup(name='vesna-alhauthproxy',
-      version='1.0.0',
+      version='1.0.1',
       description='ALH authorization proxy for OMF',
       license='GPL',
       long_description=get_long_description(),
@@ -25,7 +25,7 @@ setup(name='vesna-alhauthproxy',
       },
 
       data_files = [
-	      ('/etc/init.d', ['init-script']),
+	      ('/etc/init.d', ['vesna_alh_auth_proxy']),
       ],
 
       install_requires = [ 'vesna-alhtools', 'requests-unixsocket', 'python-daemon' ],
